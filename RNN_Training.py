6729731a3470ae7cba2ef6538b91dd884d7b22c5
LU RUI <704041404@qq.com>
2018-11-24 14:10:48 +0800

    firstupdate

---
+++ RNN_Training.py
@@ -0,0 +1,34 @@
+import torch
+from torch import nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+EPOCH = 1               # train the training data n times, to save time, we just train 1 epoch
+LR = 0.01   
+TIME_STEP = 28          # rnn time step / image height
+INPUT_SIZE = 28         # rnn input size / image width
+
+class RNN(nn.Module):
+    def __init__(self):
+        super(RNN, self).__init__()
+
+        self.rnn = nn.LSTM(         # if use nn.RNN(), it hardly learns
+            input_size=INPUT_SIZE,
+            hidden_size=64,         # rnn hidden unit
+            num_layers=1,           # number of rnn layer
+            batch_first=True,       # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
+        )
+
+        self.out = nn.Linear(64, 10)
+
+    def forward(self, x):
+        # x shape (batch, time_step, input_size)
+        # r_out shape (batch, time_step, output_size)
+        # h_n shape (n_layers, batch, hidden_size)
+        # h_c shape (n_layers, batch, hidden_size)
+        r_out, (h_n, h_c) = self.rnn(x, None)   # None represents zero initial hidden state
+
+        # choose r_out at the last time step
+        out = self.out(r_out[:, -1, :])
+        return out
